b56b9ad9c44839dc7b99f5397a734a95770509e5
minglangdu <mldu@cydu.net>
2025-07-10 23:22:24 -0700

    Add actual content lol

---
+++ play.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+declare SongsFolder="songs"
+declare SongList=("Surface.mp3" "ATSOL.mp3" "DarkMatterSuite.mp3" "Menace.mp3" "Stalemate.mp3" "DryOut.mp3")
+
+echo "Enter song name or ID: "
+read sid
+declare sname
+if [[ $sid =~ ^-?[0-9]+$  ]]; then
+	# is a song ID
+	sname=${SongList[sid]}
+else
+	# is a song name
+	if [[ $sid =~ \. ]]; then # abysmal regex use ik
+		# means there is already a file extension in name
+		sname=sid
+	else
+		sname="${sid}.mp3" # im assuming mp3, if the user wanted to play a different extension that's on them ¯\_(ツ)_/¯
+	fi
+fi # I love how this is the closing for "if"
+
+ffplay -loop 0 -i -volume 8 "${SongsFolder}/${sname}"
+exit 0
+
